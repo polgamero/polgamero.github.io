557c4ce53daa55d7f9ba70a14722aac91b7040bd
polgamero <gameropablo1@gmail.com>
2023-09-13 21:50:28 -0300

    Se agrega una landing con un mensaje de bienvenida

--- curso_react/src/App.js
+++ curso_react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer bienvenida={'¡Bienvenidos a mi Shop Online!'}/>
     </div>
   );
 }
---
+++ curso_react/src/Components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+const ItemListContainer = ({ bienvenida }) => {
+    return (
+        <div>
+            <h1>
+                {bienvenida}
+            </h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
