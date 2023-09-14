10aa4068915b041965801f188b024b09a3cc8c4b
polgamero <gameropablo1@gmail.com>
2023-09-13 21:23:56 -0300

    Se agrega componente navbar

---
+++ curso_react/src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Ecommerce</h3>
+            <div>
+                <button>Categoría 1</button>
+                <button>Categoría 2</button>
+                <button>Categoría 3</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
