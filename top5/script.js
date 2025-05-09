67e15c6bbbd411a7bb043ecd36e57100a462295b
polgamero <130258003+polgamero@users.noreply.github.com>
2025-05-09 11:17:58 -0300

    Update script.js

--- top5/script.js
+++ top5/script.js
@@ -6,18 +6,17 @@ let selectedImage = null;
 
 // Cargar imágenes iniciales
 function loadImages() {
-//  imagesContainer.innerHTML = '';
-
-//  for (let i = 1; i <= 20; i++) {
-//    const img = document.createElement("img");
-//    img.src = `https://picsum.photos/seed/${i}/200/140`;
-//    img.classList.add("img-item");
-//    img.setAttribute("draggable", "true");
-//    img.setAttribute("data-id", `img-${i}`);
+    imagesContainer.innerHTML = '';
+  for (let i = 1; i <= 20; i++) {
+    const img = document.createElement("img");
+    img.src = `https://picsum.photos/seed/${i}/200/140`;
+    img.classList.add("img-item");
+    img.setAttribute("draggable", "true");
+    img.setAttribute("data-id", `img-${i}`);
     enableInteraction(img);
-//    imagesContainer.appendChild(img);
+    imagesContainer.appendChild(img);
   }
-//}*/
+}*/
 
 // Guardar estado en localStorage
 function saveState() {
