e017c41feba42fed230ba34ab3f885bf3d5252a7
polgamero <130258003+polgamero@users.noreply.github.com>
2025-05-15 20:48:29 -0300

    Update script.js

--- botonera/script.js
+++ botonera/script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sounds = [
-    { name: "Risas", file: "https://polgamero.github.io/botonera/sounds/risas.mp3", key: "0" },
-    { name: "LOL", file: "https://polgamero.github.io/botonera/sounds/LOL.wav", key: "1" },
-    { name: "Pedo", file: "https://polgamero.github.io/botonera/sounds/pedo.wav", key: "2" },
-    { name: "Buuu", file: "https://polgamero.github.io/botonera/sounds/buuu.wav", key: "3" },
-    { name: "Risas", file: "https://polgamero.github.io/botonera/sounds/risas.mp3", key: "4" },
+    { name: "Ping Pong", file: "https://polgamero.github.io/botonera/sounds/ping.wav", key: "0" },
+    { name: "Momento Fogón", file: "https://polgamero.github.io/botonera/sounds/momento.wav", key: "1" },
+    { name: "Top 5", file: "https://polgamero.github.io/botonera/sounds/five.wav", key: "2" },
+    { name: "Lo tocás?", file: "https://polgamero.github.io/botonera/sounds/tocas.wav", key: "3" },
+    { name: "Playlist prohibida", file: "https://polgamero.github.io/botonera/sounds/playlist.wav", key: "4" },
     { name: "LOL1", file: "https://polgamero.github.io/botonera/sounds/LOL.wav", key: "5" },
     { name: "Pedo1", file: "https://polgamero.github.io/botonera/sounds/pedo.wav", key: "6" },
     { name: "Buuu1", file: "https://polgamero.github.io/botonera/sounds/buuu.wav", key: "7" },
-    { name: "Pedo1", file: "https://polgamero.github.io/botonera/sounds/pedo.wav", key: "8" },
+    { name: "Risas", file: "https://polgamero.github.io/botonera/sounds/risas.wav", key: "8" },
     { name: "Buuu1", file: "https://polgamero.github.io/botonera/sounds/buuu.wav", key: "9" }
 ];
 
