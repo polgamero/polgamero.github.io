1bb148ba53233a3c75098feb1908503d1a192b6e
polgamero <130258003+polgamero@users.noreply.github.com>
2026-08-06 22:46:34 -0300

    Update ui.js

--- argentinia/js/ui.js
+++ argentinia/js/ui.js
@@ -318,7 +318,7 @@ export function createCardElement(itemObj, isTapped = false, isLocal = true, ind
     const KEYWORD_LABELS = { 
       flying: '🕊️ Vuela', trample: '🐘 Arrolla', hexproof: '🛡️ Intocable', haste: '⚡ Prisa', 
       menace: '👥 Amenaza', vigilance: '👁️ Vigilancia', reach: '🏹 Alcance', defender: '🧱 Defensora',
-      lifelink: '❤️ Vínculo vital', deathtouch: '💀 Toque mortal', firststrike: '⚔️ Primer golpe'
+      lifelink: '❤️ Vínculo vital', deathtouch: '💀 Toque mortal', firststrike: '🗡️ Primer golpe', doublestrike: '⚔️ Doble golpe'
     };
       
     const keywordsHTML = effKeywords.length > 0
