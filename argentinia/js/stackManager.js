ea8de9268016fa0c293aea583f063a681cd98096
polgamero <130258003+polgamero@users.noreply.github.com>
2026-08-08 18:35:26 -0300

    Update stackManager.js

--- argentinia/js/stackManager.js
+++ argentinia/js/stackManager.js
@@ -396,7 +396,7 @@ async function executeStackItem(item) {
             name: effectToApply.tokenName || 'Ficha',
             type: 'Criatura Token',
             manaCost: null,
-            image: card.image,
+            image: effectToApply.image,
             cmc: 0,
             rarity: 'Common',
             colors: card.colors || [],
